a93987c9f53691c2f2ccdea90eb88533d34451b3
Stefan Dantchev <sdantchev@gvcgroup.com>
2019-03-22 18:31:53 +0200

    merge

--- src/app/oddscontainer/gridcontainer/preview/preview.component.ts
+++ src/app/oddscontainer/gridcontainer/preview/preview.component.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { SportsEvent } from '../../../models/SportsEvent.model';
 import { SportEventService } from '../../../sport-event.service';
-=======
-import { Component, OnInit, Input } from '@angular/core';
-import { SportsEvent } from 'src/app/models/SportsEvent.model';
->>>>>>> 2c897cb5369e34515aed2c08e636f31359aff140
 
 @Component({
   selector: 'app-preview',
@@ -15,15 +10,9 @@ import { SportsEvent } from 'src/app/models/SportsEvent.model';
 export class PreviewComponent implements OnInit {
   sportEvents: SportsEvent[];
 
-<<<<<<< HEAD
   constructor(public sportEventService: SportEventService) { 
     this.sportEvents = this.sportEventService.getAll();
   }
-=======
-  @Input() sportEvents: SportsEvent[];
-
-  constructor() { }
->>>>>>> 2c897cb5369e34515aed2c08e636f31359aff140
 
   ngOnInit() {
   }
